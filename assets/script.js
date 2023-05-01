8620db8145459ff6f0358b19f30cfc19a995c41e
Fitim Tahiraj <fitimtahiraj@fitims-air.myfiosgateway.com>
2023-05-01 13:55:47 -0400

    Added giff image

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,16 @@
 var playerEl = document.querySelector("#search");
+var image = document.querySelector("#image");
 var firstNamePlayer = document.querySelector("#first-name");
 var lastNamePlayer = document.querySelector("#last-name");
+var burgerIcon = document.querySelector("#burger");
+var navbarMenu = document.querySelector("#nav-links")
+
+//Mobile Menu
+burgerIcon.addEventListener("click", getMenu);
+
+function getMenu() {
+    navbarMenu.classList.toggle("is-active")
+}
 
 // var firstName = firstNamePlayer.value
 // var lastName = lastNamePlayer.value
@@ -8,8 +18,14 @@ var stats = [];
 
 playerEl.addEventListener("click", function () {
   var firstName = firstNamePlayer.value
+  if(firstName === "") {
+    location.reload();
+  }
   console.log(firstName);
   var lastName = lastNamePlayer.value
+  if(lastName === "") {
+    location.reload();
+  }
   console.log(lastName);
 fetch(`https://www.balldontlie.io/api/v1/players?search=${firstName}+${lastName}`)
   .then(function (answer) {
@@ -30,12 +46,18 @@ fetch(`https://www.balldontlie.io/api/v1/players?search=${firstName}+${lastName}
         console.log(info);
     })
   })
-  fetch(`http://api.giphy.com/v1/gifs/search?q=${firstName}+${lastName}&api_key=fxEW2ambgr9GHTzx6iXmXJKl5Zss1Fma&limit=5`)
+  fetch(`http://api.giphy.com/v1/gifs/search?q=${firstName}+${lastName}&api_key=fxEW2ambgr9GHTzx6iXmXJKl5Zss1Fma&limit=10`)
   .then(function (response) {
     return response.json();
   })
   .then(function (info) {
     console.log(info);
-    console.log(info.data[0].images.original.url);
+    var randomItem = info.data[Math.floor(Math.random()*info.data.length)];
+    // console.log(randomItem.images.original.url);
+    var giffy = randomItem.images.original.url;
+    console.log(giffy);
+    var ghiphy = document.createElement("img");
+    ghiphy.src = giffy;
+    image.append(ghiphy)
   })
 });
